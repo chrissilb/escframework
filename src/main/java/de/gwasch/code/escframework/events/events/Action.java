d38b4aa7868a27e53a906a1a02fdb07f947d6fed
chrissilb <christian.silberbauer@gwasch.de>
2023-09-28 09:09:17 +0200

    update docs

--- src/main/java/de/gwasch/code/escframework/events/events/Action.java
+++ src/main/java/de/gwasch/code/escframework/events/events/Action.java
@@ -5,7 +5,6 @@ package de.gwasch.code.escframework.events.events;
  * <p>
  * An {@code Action} must be reusable, e.g. for reenqueuing in {@link Scheduler}. 
  * That means that after {@link #cancel()} the {@link #execute()} method can be called again.
- * 
  */
 
 public interface Action extends Event {
