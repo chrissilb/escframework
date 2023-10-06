ffb52a03d8e6f859a664a81874d047e6bf8e529a
chrissilb <christian.silberbauer@gwasch.de>
2023-10-06 15:49:25 +0200

    update docs

--- src/main/java/de/gwasch/code/escframework/states/functions/Function.java
+++ src/main/java/de/gwasch/code/escframework/states/functions/Function.java
@@ -1,5 +1,7 @@
 package de.gwasch.code.escframework.states.functions;
 
+import de.gwasch.code.escframework.states.states.FunctionState;
+
 /**
  * A {@code Function} can be injected to a {@link FunctionState}. It provides a {@code State} value according to its
  * function algorithm. Those implementations typically use {@code State} parameters.
